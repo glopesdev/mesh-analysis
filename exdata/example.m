ec83a5cbe0c78a99b1a6d405b4cd51c55533355e
Eric E. J. DeWitt <edewitt@hampshire.edu>
2016-05-04 19:13:30 +0100

    modified examples to explicitly specify sample frequency

--- exdata/example.m
+++ exdata/example.m
@@ -9,6 +9,6 @@
 addpath('matmesh');
 
 tuna_packet = get_tuna_packets('exdata/tuna.bin');
-[tuna_data tuna_state tuna_channelmap] = packet_to_ndarray(tuna_packet);
+[tuna_data tuna_state tuna_channelmap] = packet_to_ndarray(tuna_packet, 200);
 
 plot(squeeze(tuna_data(1601:2800, 1, 1:3)));
--- exdata/example.py
+++ exdata/example.py
@@ -10,7 +10,7 @@
 from pymesh import tuna
 from matplotlib import pyplot as plt
 tuna_frames = tuna.fromfile('exdata/tuna.bin')
-tuna_timeline = tuna.timeline(tuna_frames)
+tuna_timeline = tuna.timeline(tuna_frames, 200)
 
 plt.plot(tuna_timeline[0][1600:2800,0,0:3])
 plt.show()
